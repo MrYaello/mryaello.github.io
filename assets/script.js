82ec3470c6ebf4d15f2b181f8948f6a303b39ef0
Yael Lozano <lozanoy6@gmail.com>
2021-02-13 20:46:16 -0600

    Popup windows update Logo bug fix

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,14 @@
 let flipcardsButton = document.getElementById("flipcardsButton");
 let closeButtonFlipcards = document.getElementById("close-window-flipcards");
 let dencryptButton = document.getElementById("dencryptButton");
-let closeButtonDencrypt = document.getElementById("close-window-dencrypt");
+let closeButtonDencrypt = document.getElementById("close-window-dencrypt")
+let coffemachineButton = document.getElementById("coffemachineButton");
+let closeButtonCoffemachine = document.getElementById("close-window-coffemachine");
+/*
+let javafyButton = document.getElementById("javafyButton");
+let closeButtonJavafy = document.getElementById("close-window-javafy");
+*/
+
 
 flipcardsButton.addEventListener('click', function() {
     document.getElementById("flipcards").classList.toggle("visible");
@@ -26,5 +33,31 @@ closeButtonDencrypt.addEventListener('click', function() {
     document.getElementById("overlay").classList.toggle("visible");
     document.getElementById("body").classList.toggle("modal-open");
 });
+
+coffemachineButton.addEventListener('click', function() {
+    document.getElementById("coffemachine").classList.toggle("visible");
+    document.getElementById("overlay").classList.toggle("visible");
+    document.getElementById("body").classList.toggle("modal-open");
+});
+
+closeButtonCoffemachine.addEventListener('click', function() {
+    document.getElementById("coffemachine").classList.toggle("visible");
+    document.getElementById("overlay").classList.toggle("visible");
+    document.getElementById("body").classList.toggle("modal-open");
+});
+
+/*
+javafyButton.addEventListener('click', function() {
+    document.getElementById("javafy").classList.toggle("visible");
+    document.getElementById("overlay").classList.toggle("visible");
+    document.getElementById("body").classList.toggle("modal-open");
+});
+
+closeButtonJavafy.addEventListener('click', function() {
+    document.getElementById("javafy").classList.toggle("visible");
+    document.getElementById("overlay").classList.toggle("visible");
+    document.getElementById("body").classList.toggle("modal-open");
+});
+*/
 
 
